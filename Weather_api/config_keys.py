7c758508dd3cc8bb155f6f3398eca7d074b71261
JosiahR89 <82723815+JosiahR89@users.noreply.github.com>
2021-06-23 22:09:25 -0500

    Update

---
+++ Weather_api/config_keys.py
@@ -0,0 +1,7 @@
+#place all API keys here with different names
+from requests import api
+
+
+omdb_key = "cf723643" #OMDB
+weather_key = "b97468fbe70ad9deb27cb5f203b90f4f" #Weather
+gkey = "AIzaSyBI1wK4n0vuqudV400attd9ZyV11hvkqu4"
